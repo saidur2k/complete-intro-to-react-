2a649eb78e42f82ff59fa3bdc0b99a49311a3e97
Saidur <saidur@rudias.com>
2016-11-11 00:35:20 +1100

    Refactor to add Header.jsx

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,14 +1,22 @@
-var React = require('react')
+const React = require('react')
+const Header = require('./Header')
 
 class Details extends React.Component
 {
   render () {
+    const params = this.props.params || {}
+    const { title, description, year, poster, trailer } = params
     return <div className="container">
-      <pre>
-        <code>
-          {JSON.stringify(this.props.params, null, 4)}
-        </code>
-      </pre>
+      <Header />
+      <div className="video-info">
+        <h1 className="video-title">{title}</h1>
+        <h2 className="video-year">({year})</h2>
+        <img src={`public/img/posters/${poster}`} className="video-poster" />
+        <p className="video-description">{description}</p>
+      </div>
+      <div className="video-container">
+        <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`} frameBorder="0" allowFullScreen className="src"></iframe>
+      </div>
     </div>
   }
 }
@@ -16,4 +24,5 @@ class Details extends React.Component
 Details.propTypes = {
   params: React.PropTypes.object
 }
+
 module.exports = Details
